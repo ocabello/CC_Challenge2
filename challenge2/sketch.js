c8766541c7ed3c45888498562b61c4ae00ad5e24
ocabello <ocabello2@gmail.com>
2016-11-09 15:27:11 -0500

    changed text

--- challenge2/sketch.js
+++ challenge2/sketch.js
@@ -5,7 +5,12 @@ var first, last;
 var tokens = "?,. \':;";
 
 var parsed;
-var counter = 0;
+var counter1 = 0;
+var counter2 = 0;
+var counter3 = 0;
+var counter4 = 0;
+var counter5 = 0;
+
 var x = 0;
 var y = 0;
 
@@ -27,41 +32,45 @@ function draw() {
 
   for (var i = 0; i < parsed.length; i++) {
     //console.log(parsed[i]);
-    if (parsed[i] == "immigration") {
+    if (parsed[i] == "immigrants") {
 
       //console.log(parsed[i]);
-      counter++;
-      console.log(counter);
-     // for(var j = 0; j < counter; j++){
-        var shape = new Visual(random(0,500), random(0,500));
-        shape.show(color(0, 0, 0), random(0,6));
-    //  }
+      counter1++;
+      //console.log(counter1);
+      var shape = new Visual(random(0,width), random(0,height));
+      shape.show(color(0), random(0,6));
+
       //fill(0);
-      //wordFormat(parsed[i], counter);
+      //wordFormat(parsed[i], counter1);
     }
     
     if (parsed[i] == "illegal"){
-      counter++;
+      counter2++;
+      //console.log(counter2);
       fill(255, 255, 0);
-      wordFormat(parsed[i], counter);
+      wordFormat(parsed[i], counter2*2);
     }
     
-    if (parsed[i] == "murder"){
-      counter++;
+    if (parsed[i] == "violence"){
+      console.log(parsed[i]);
+      counter3++;
+      //console.log(counter3);
       fill (253, 0, 0);
-      wordFormat(parsed[i], counter);
+      wordFormat(parsed[i], counter3*2);
     }
     
-    if (parsed[i] == "refugees"){
-      counter++;
+    if (parsed[i] == "america"){
+      counter4++;
+      //console.log(counter4);
       fill (10, 50, 80);
-      wordFormat(parsed[i], counter);
+      wordFormat(parsed[i], counter4*2);
     }
     
-    if (parsed[i] == "safety"){
-      counter++;
+    if (parsed[i] == "order"){
+      counter5++;
+      //console.log(counter5);
       fill (100, 200, 100);
-      wordFormat(parsed[i], counter);
+      wordFormat(parsed[i], counter5*2);
     }
     
   }
@@ -71,7 +80,7 @@ function loadContent() {
   var stringarray = join(webtext, "");
   //console.log(stringarray);
 
-  first = stringarray.indexOf("Wow.");
+  first = stringarray.indexOf("Friends");
 
   last = stringarray.lastIndexOf("important.");
 
