95c932ad95844c891c4b254dc2d3b5167250b634
ocabello <ocabello2@gmail.com>
2016-11-13 02:27:34 -0500

    added interaction and comments

--- challenge2/sketch.js
+++ challenge2/sketch.js
@@ -14,6 +14,8 @@ var counter5 = 0;
 var x = 0;
 var y = 0;
 
+value = 0;
+
 function preload() {
   webtext = loadStrings("data/trumpspeech.txt");
 
@@ -23,60 +25,27 @@ function preload() {
 function setup() {
   createCanvas(500, 500);
   background(255, 204, 153);
-  noLoop();
+  //noLoop();
   loadContent();
 
 }
 
 function draw() {
 
-  for (var i = 0; i < parsed.length; i++) {
-    //console.log(parsed[i]);
-    if (parsed[i] == "immigrants") {
+  background(255, 204, 153);
 
-      //console.log(parsed[i]);
-      counter1++;
-      //console.log(counter1);
-      var shape = new Visual(random(0,width), random(0,height));
-      shape.show(color(0), random(0,6));
 
-      //fill(0);
-      //wordFormat(parsed[i], counter1);
+  generateWords();
+
+  if (keyIsPressed) {  //shapes are generated if m key is pressed
+    if (key == 'm') {
+      generateShapes();
     }
-    
-    if (parsed[i] == "illegal"){
-      counter2++;
-      //console.log(counter2);
-      fill(255, 255, 0);
-      wordFormat(parsed[i], counter2*2);
-    }
-    
-    if (parsed[i] == "violence"){
-      console.log(parsed[i]);
-      counter3++;
-      //console.log(counter3);
-      fill (253, 0, 0);
-      wordFormat(parsed[i], counter3*2);
-    }
-    
-    if (parsed[i] == "america"){
-      counter4++;
-      //console.log(counter4);
-      fill (10, 50, 80);
-      wordFormat(parsed[i], counter4*2);
-    }
-    
-    if (parsed[i] == "order"){
-      counter5++;
-      //console.log(counter5);
-      fill (100, 200, 100);
-      wordFormat(parsed[i], counter5*2);
-    }
-    
   }
+
 }
 
-function loadContent() {
+function loadContent() {                       //loads and parses data from speech
   var stringarray = join(webtext, "");
   //console.log(stringarray);
 
@@ -93,7 +62,7 @@ function loadContent() {
 
 }
 
-function wordFormat(word, size) {
+function wordFormat(word, size) {  //adjust word size and position in canvas
   var wordSize = size;
   var word = word;
 
@@ -110,5 +79,89 @@ function wordFormat(word, size) {
 
   if (y > height) {
     x = 0;
+  }
+}
+
+function generateWords() {
+  // noLoop();
+  frameRate(0.5);                          //decreased frameRate to make words seem like they're slowly multiplying
+  for (var i = 0; i < parsed.length; i++) {  //
+    //console.log(parsed[i]);
+    if (parsed[i] == "immigrants") {
+      //console.log(parsed[i]);
+      counter1++;
+      //console.log(counter1);
+      fill(0);
+      wordFormat(parsed[i], counter1);     //words adopt size of counter (the more frequent the word, the bigger it is)
+
+    }
+
+    if (parsed[i] == "illegal") {
+      counter2++;
+      //console.log(counter2);
+
+      fill(255, 255, 0);
+      wordFormat(parsed[i], counter2 * 2);
+    }
+
+    if (parsed[i] == "violence") {
+      // console.log(parsed[i]);
+      counter3++;
+      //console.log(counter3);
+
+      fill(253, 0, 0);
+      wordFormat(parsed[i], counter3 * 2);
+    }
+
+    if (parsed[i] == "america") {
+      counter4++;
+      //console.log(counter4);
+
+      fill(10, 50, 80);
+      wordFormat(parsed[i], counter4 * 2);
+
+    }
+
+    if (parsed[i] == "order") {
+      counter5++;
+      //console.log(counter5);
+
+      fill(100, 200, 100);
+      wordFormat(parsed[i], counter5 * 2);
+
+    }
+
+  }
+}
+
+function generateShapes() {         //generates random shapes taken from the visual class
+  //loop();
+  frameRate(5);                     //increased frameRate to create feeling of being overwhelmed
+
+  for (var i = 0; i < parsed.length; i++) {
+    if (parsed[i] == "immigrants") {                                  //shape color matches corresponding shape color
+      var shape1 = new Visual(random(0, width), random(0, height));   //in order to make the connection between both
+      shape1.show(color(0), random(0, 6));
+    }
+
+    if (parsed[i] == "illegal") {
+      var shape2 = new Visual(random(0, width), random(height));
+      shape2.show(color(255, 255, 0), random(0, 6));
+    }
+
+    if (parsed[i] == "violence") {
+      var shape3 = new Visual(random(0, width), random(0, height));
+      shape3.show(color(253, 0, 0), random(0, 6));
+    }
+
+    if (parsed[i] == "america") {
+      var shape4 = new Visual(random(0, width), random(0, height));
+      shape4.show(color(10, 50, 80), random(0, 6));
+    }
+
+    if (parsed[i] == "order") {
+      var shape5 = new Visual(random(0, width), random(0, height));
+      shape5.show(color(100, 200, 100), random(0, 6));
+    }
   }
 }
--- challenge2/visual.js
+++ challenge2/visual.js
@@ -1,16 +1,18 @@
-function Visual(locX, locY){
+function Visual(locX, locY){   //visual class to create random sized shapes
   this.x = locX;
   this.y = locY;
   
-  this.show = function(c, vtxnum){
+  this.show = function(c, vtxnum){     //takes vertex number and color as parameters
     stroke(4);
     fill(c)
     beginShape();
     for (var i = 0; i< vtxnum; i++){
     vertex(this.x, this.y);
-    this.x += random(0,20);
-    this.y += random(0,50);
+    this.x += random(20,100);       //random position of vertices
+    this.y += random(20,80);
     }
-    endShape(CLOSE);
+    endShape(CLOSE);               //connects all vertices
   }
+  
+
 }
